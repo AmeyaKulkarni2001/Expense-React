43718b9bae60461577f4bb9c8ec89a479ecb973e
A K <ameya.2000k@gmail.com>
2021-05-20 20:57:59 +0530

    Refactoring Basic code

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,8 +1,8 @@
 import ExpenseItem from './ExpenseItem';
 import "./Expense.css";
-import Card from "./UI/Card";
+import Card from "../UI/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
